f646b36e2986666aa815ace2720a42dc05a27b03
Erik Olson <xorgnak@gmail.com>
2020-03-16 21:14:08 -0500

    maybe?

--- mango/lib/devconf.rb
+++ mango/lib/devconf.rb
@@ -43,19 +43,15 @@ class Arduino
     Process.detach(
       fork {
         while true do
-          i = @sp.readlines
-          if i[0] != nil
-#            Redis.new().publish("DEBUG.i<", "#{i}")
-            if /\{.*}/.match(i[0])
-              Redis.new.publish "DEBUG.input.stats", input(i[0]).stats
-            elsif /.*}/.match(i[0])
-              ix << i[0]
-              Redis.new.publish "DEBUG.stats", input(ix.join('')).stats
+          while (i = @sp.gets) do       # see note 2
+            if /\n/.match(i)
+              ix << i
+              if /^.+ ..:..:..:..:..:.. ..:..:..:..:..:.. #......\r\n$/.match(ix.join(""))
+                Redis.new.publish("irc", ix.join(""))
+              end
               ix = []
-            elsif /.*\{.*/.match(i[0])
-              ix = [ i[0].gsub(/.*\{/, '{') ]
             else
-              ix << i[0]
+              ix << i
             end
           end
         end
@@ -70,43 +66,6 @@ class Arduino
   def record_zap l, r
     Redis::HashKey.new("lt:#{r}:to").incr(l)                                                                                         
     Redis::HashKey.new("lt:#{l}:out").incr(r)
-  end
-  def input i
-#    Redis.new().publish("DEBUG.i", "#{i}")
-    begin
-      h = JSON.parse(i)
-    rescue => e
-#      Redis.new().publish("DEBUG.error", "#{e}")
-      h = {}
-    end
-    if h.keys.length > 0
-      @aura = h['local_aura']
-#      Redis::SortedSet.new("lt:#{h['mac']}:#{h['direction']}").incr(h['node'])
-      if h['direction'] == "out"
-        @hits[h['remote_mac']] += 1
-        record_zap h['local_mac'], h['remote_mac']
-          push beacon: %[<p class='#{h['direction']}'>You hit #{h['remote_mac']} (#{h['remote_strength'].to_i.abs})</p>]
-      elsif h['direction'] == "in"
-        @hit[h['remote_mac']] += 1
-        record_zap h['remote_mac'], h['local_mac']
-          push beacon: %[<p class='#{h['direction']}'>#{h['remote_mac']} hit you!</p>]
-      else
-        if h['remoe_ap'] != "me"
-          hh = Redis::HashKey.new("ap:#{h['remote_ap']}")
-          @scan[h['remote_mac']] = {}
-          @scan[h['remote_mac']]['ap'] = hh['ap'] = h['remote_ap']
-          @scan[h['remote_mac']]['strength'] = hh['strength'] = h['remote_strength']
-          Redis::HashKey.new("lt:scan").incr(h['remote_ap'])
-            push beacon: %[<p class='#{h['direction']}'>#{h['remote_ap']} (#{h['remote_strength'].to_i.abs}) #{h['direction']}</p>]
-        end
-      end
-      @last['direction'] = h['direction']
-      @last['ap'] = h['remote_ap']
-      @last['strength'] = h['remote_strength']
-      @last['mac'] = h['remote_mac']
-      #      Redis.new().publish("DEBUG.h", "#{h}")
-    end
-    return self
   end
   def stats
     { :aura => @aura, :last => @last, :in => @hit, :out => @hits, :scan => @scan }
